807dd55b8f240b36d15634af08ca2f543e310136
RafinYo <mijanulislamrafinha@gmail.com>
2024-10-21 10:51:56 +0600

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -3,7 +3,7 @@ loadstring(game:HttpGet("https://github.com/dawid-scripts/Fluent/releases/latest
 local Fluent = loadstring(game:HttpGet("https://github.com/dawid-scripts/Fluent/releases/latest/download/main.lua"))()
 
 local Window = Fluent:CreateWindow({
-    Name = "Xyris Hub - Concave World Free Jungle",
+    Title = "Xyris Hub - Concave World Free Jungle",
     Subtitle = "Made By Xyris Hub Team",
     TabWidth = 180,
     Size = UDim2.fromOffset(450, 240),
@@ -31,7 +31,7 @@ MainTab:CreateButton({
 })
 
 local ToggleOption = MainTab:CreateToggle({
-    Name = "Loop Get Event Item",
+    Name = "The Loop Get Event Item",
     Default = false,
     Callback = function(state)
         if state then
